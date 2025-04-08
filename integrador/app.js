1f49c1fc02021c8d25fa9c48af5c41d0f309949e
eemmmmma <emagnin@udesa.edu.ar>
2025-04-08 11:26:14 -0300

    arreglando los views

--- integrador/app.js
+++ integrador/app.js
@@ -1,47 +1,50 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var loginRouter = require('./routes/login');
-var registerRouter = require('./routes/register');
-var productsRouter = require('./routes/products');
+// Rutas
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const loginRouter = require('./routes/login');
+const registerRouter = require('./routes/register');
+const productsRouter = require('./routes/products');
+const productsAddRouter = require('./routes/products-add');
+const profileRouter = require('./routes/profile');
+const searchResultsRouter = require('./routes/search-results');
 
-var app = express();
+const app = express();
 
-// view engine setup
+// View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
+// Rutas principales
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/', loginRouter);
-app.use('/', registerRouter);
-app.use('/', productsRouter);
+app.use('/login', loginRouter);
+app.use('/register', registerRouter);
+app.use('/products', productsRouter);
+app.use('/products-add', productsAddRouter);
+app.use('/profile', profileRouter);
+app.use('/search', searchResultsRouter); // ruta para el formulario de búsqueda
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// Catch 404 y manejador de errores
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
+ 
   res.status(err.status || 500);
   res.render('error');
 });
