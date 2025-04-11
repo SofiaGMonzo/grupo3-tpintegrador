11ff5b3c34704f3ac25da50f80c0a69042a90d98
sofia <smonzo@udesa.edu.ar>
2025-04-11 15:28:21 -0300

    termine el punto 1 y arranque el css del trabajo

--- integrador/controllers/mainController.js
+++ integrador/controllers/mainController.js
@@ -8,16 +8,7 @@ const mainController = {
     },
     searchResults: function (req, res) {
         return res.render('search-results')
-      },
-    /*searchResults: function (req, res) {
-        let search = req.params.search;
-    
-        let productos = baseDatos.productos;
-    
-        let resultados = COMPLETAR 
-    
-        res.render('search-results', { search, resultados });
-      }*/
+      }
 };
 
 module.exports = mainController;
