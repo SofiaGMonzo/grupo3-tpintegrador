dbd6a987cb17298ce90756db437c62c34a935af3
BrisaPereyra23 <bpereyra@udesa.edu.ar>
2025-05-25 19:54:51 -0300

    comente cosas para el 7

--- integrador/routes/index.js
+++ integrador/routes/index.js
@@ -8,4 +8,9 @@ router.get('/search-results', mainController.searchResults);
 
 module.exports = router;
 
+/* PARA EL PUNTO 7
+const productsController = require("../controllers/productsController");
+
+router.get('/', productsController.index);
+*/
 
